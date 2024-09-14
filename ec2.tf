cf520130b52e85c7c1d78ba294c3f41fa80d717b
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-15 00:54:29 +0530

    * to create security group * to associate security group to the instance

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,28 @@
 resource "aws_instance" "web" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-
+  vpc_security_group_ids = "[aws_security_group.allow_tls.id]"
   tags = {
     Name = "test"
   }
 }
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  ingress {
+      from_port        = 22
+      to_port          = 22
+      protocol         = "ssh"
+      cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
 
-# ami-090252cbe067a9e58
+  }
+  tags = {
+    Name = "custom_security"
+  }
+}
