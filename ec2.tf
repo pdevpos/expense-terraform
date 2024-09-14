146799b9c9d04b8839496089d9426a5617be5eb5
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-15 01:03:06 +0530

    * to create security group * to associate security group to the instance

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [aws_security_group.custom_sec_group.id]
   tags = {
     Name = "test"
   }
