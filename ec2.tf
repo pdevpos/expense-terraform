422d36f1fd659b2e00a9cee8f7ff25a0949164be
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-15 21:47:23 +0530

    * added variables * pass values dynamically

--- ec2.tf
+++ ec2.tf
@@ -1,28 +1,29 @@
 resource "aws_instance" "web" {
   ami           = var.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.custom_sec_group.id]
   tags = {
-    Name = "test"
+    Name = var.tags_name
   }
 }
 resource "aws_security_group" "custom_sec_group" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  name        = var.aws_sg
+  description = var.aws_sg_descrp
+
   ingress {
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      from_port        = var.from_port
+      to_port          = var.to_port
+      protocol         = var.protocol
+      cidr_blocks      = var.cidr_blocks
   }
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = var.cidr_blocks
 
   }
   tags = {
-    Name = "custom_security"
+    Name = var.custom_tag
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "image_id"{
     type = string
     default = "ami-09c813fb71547fc4f"
  }
+ variable "instance_type"
+ {
+     default = "t2.micro"
+  }
+  variable "tags_name"
+  {
+  default = "test"
+  }
+  variable "aws_sg"
+  {
+  default = "allow_tls"
+  }
+  variable "aws_sg_descrp"
+  {
+  default = "Allow TLS inbound traffic and all outbound traffic"
+  }
+  variable "from_port"
+  {
+  default = 22
+  }
+  variable "to_port"
+  {
+  default = 22
+  }
+  variable "protocol"
+  {
+  default = "tcp"
+  }
+  variable "cidr_blocks"
+  {
+  default =  ["0.0.0.0/0"]
+  }
+  variable "sg_tag"
+  {
+  default = "custome_sg"
+  }
