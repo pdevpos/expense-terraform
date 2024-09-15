b411df9ffd14b6c2848a69ed1841c1f66655a2e6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-15 22:10:39 +0530

    added spot instance

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,12 @@
 resource "aws_instance" "web" {
   ami           = var.image_id
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      spot_instance_type = "persistent"
+      instance_interruption_behavior = "stop"
+    }
+    }
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.custom_sec_group.id]
   tags = {
