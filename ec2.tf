e5ef7133aabb84602a5357f48a1803ee764cac26
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-15 21:35:18 +0530

    pass values dynamically

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = var.image_id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.custom_sec_group.id]
   tags = {
     Name = "test"
