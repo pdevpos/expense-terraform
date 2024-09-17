9aafd3e9e83609b7c2a80d2c66166bda373a1974
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-17 23:58:29 +0530

    create route53

---
+++ route53/aws_rt53.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_record" "aws_route" {
+  zone_id = local.zone_id
+  name    = var.instance_name[count.index]
+  type    = "A"
+  ttl     = 300
+  //records = [var.instance_name[count.index] == "frontend" ? "pdevops72.online" : var.instance_name[count.index].pdevops72.online]
+}
+
