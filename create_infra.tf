4a37c965505884befed39114251dd865c2585e65
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-18 01:01:40 +0530

    *added aws_route53 records * with ip address

--- create_infra.tf
+++ create_infra.tf
@@ -35,9 +35,8 @@ resource "aws_security_group" "allow_tls" {
 resource "aws_route53_record" "aws_route" {
   count = length(var.instance_name)
   zone_id = local.zone_id
-  name = var.instance_name[count.index] == "frontend"? var.dns_name : "${var.instance_name}.${var.dns_name}"
+  name = var.instance_name[count.index] == "frontend"? var.dns_name : "${var.instance_name[count.index]}.${var.dns_name}"
   type    = "A"
   ttl     = 300
-  records = [
-      var.instance_name[count.index] == "frontend" ? aws_instance.expense[count.index].public_ip : aws_instance.expense[count.index].private_ip]
+  records = var.instance_name[count.index] == "frontend" ? aws_instance.expense[count.index].public_ip : aws_instance.expense[count.index].private_ip
 }
