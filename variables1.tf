880a7d2c6932d04c826446ef2d0344befbb9bcd4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-09-26 16:43:30 +0530

    comment unnecessary code

---
+++ variables1.tf
@@ -0,0 +1,40 @@
+# variable "image_id"{
+#   default = "ami-09c813fb71547fc4f"
+#  }
+#  variable "instance_type"{
+#    default = "t3.micro"
+#   }
+#   variable "tags_name"{
+#     default = {
+#       Project="Expense"
+#       Environment="Dev"
+#       Module="DB"
+#       Name="DB"
+#     }
+#
+#   }
+#   variable "aws_sg"{
+#     default = "allow_tls"
+#   }
+#   variable "aws_sg_descrp"{
+#     default = "Allow TLS inbound traffic and all outbound traffic"
+#   }
+#   variable "from_port"{
+#     default = 22
+#   }
+#   variable "to_port"{
+#     default = 22
+#   }
+#   variable "protocol"{
+#     default = "tcp"
+#   }
+#   variable "cidr_blocks"{
+#     default =  ["0.0.0.0/0"]
+#   }
+#   variable "sg_tag"{
+#     default = "custome_sg"
+#   }
+#   variable "instance_name"{
+#     default = ["db","backend","frontend"]
+#
+#   }
